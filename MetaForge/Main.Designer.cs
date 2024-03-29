8147850f3e269abe6f6209b57c2d1eb928ba3efc
ArmandoSaenz <43352833+ArmandoSaenz@users.noreply.github.com>
2024-03-29 14:54:07 -0600

    Agregar archivos de proyecto.

---
+++ MetaForge/Main.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace MetaForge
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.btnUrl = new System.Windows.Forms.Button();
+            this.tbURLImage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvMetadatos = new System.Windows.Forms.DataGridView();
+            this.colMetadata = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lMaps = new System.Windows.Forms.Label();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMetadatos)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pbImage);
+            this.groupBox1.Controls.Add(this.btnUrl);
+            this.groupBox1.Controls.Add(this.tbURLImage);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(383, 335);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Imagen";
+            // 
+            // pbImage
+            // 
+            this.pbImage.BackgroundImage = global::MetaForge.Properties.Resources.Camera_icon;
+            this.pbImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImage.Location = new System.Drawing.Point(9, 58);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(365, 271);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImage.TabIndex = 3;
+            this.pbImage.TabStop = false;
+            this.pbImage.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.pbImage_LoadCompleted);
+            this.pbImage.Validated += new System.EventHandler(this.pbImage_Validated);
+            // 
+            // btnUrl
+            // 
+            this.btnUrl.Location = new System.Drawing.Point(338, 30);
+            this.btnUrl.Name = "btnUrl";
+            this.btnUrl.Size = new System.Drawing.Size(36, 23);
+            this.btnUrl.TabIndex = 2;
+            this.btnUrl.Text = "...";
+            this.btnUrl.UseVisualStyleBackColor = true;
+            this.btnUrl.Click += new System.EventHandler(this.btnUrl_Click);
+            // 
+            // tbURLImage
+            // 
+            this.tbURLImage.Location = new System.Drawing.Point(9, 32);
+            this.tbURLImage.Name = "tbURLImage";
+            this.tbURLImage.Size = new System.Drawing.Size(323, 20);
+            this.tbURLImage.TabIndex = 1;
+            this.tbURLImage.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbURLImage_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dirección de la imagen:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvMetadatos);
+            this.groupBox2.Location = new System.Drawing.Point(401, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(528, 464);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Metadatos";
+            // 
+            // dgvMetadatos
+            // 
+            this.dgvMetadatos.AllowUserToAddRows = false;
+            this.dgvMetadatos.AllowUserToDeleteRows = false;
+            this.dgvMetadatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMetadatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colMetadata,
+            this.colValue});
+            this.dgvMetadatos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvMetadatos.Location = new System.Drawing.Point(3, 16);
+            this.dgvMetadatos.Name = "dgvMetadatos";
+            this.dgvMetadatos.RowHeadersVisible = false;
+            this.dgvMetadatos.Size = new System.Drawing.Size(522, 445);
+            this.dgvMetadatos.TabIndex = 0;
+            // 
+            // colMetadata
+            // 
+            this.colMetadata.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.colMetadata.HeaderText = "Metadato";
+            this.colMetadata.Name = "colMetadata";
+            this.colMetadata.Width = 77;
+            // 
+            // colValue
+            // 
+            this.colValue.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.colValue.HeaderText = "Valor";
+            this.colValue.Name = "colValue";
+            this.colValue.Width = 56;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Imágenes (*.jpg;*.jpeg;*.png;*.bmp;*.gif)|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnCopy);
+            this.groupBox3.Controls.Add(this.lMaps);
+            this.groupBox3.Location = new System.Drawing.Point(12, 353);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(383, 123);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Liga de google maps";
+            // 
+            // lMaps
+            // 
+            this.lMaps.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lMaps.Location = new System.Drawing.Point(6, 16);
+            this.lMaps.Name = "lMaps";
+            this.lMaps.Size = new System.Drawing.Size(368, 52);
+            this.lMaps.TabIndex = 0;
+            this.lMaps.Text = "SIN UBICACION";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(299, 94);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 1;
+            this.btnCopy.Text = "Copiar";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Active = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 488);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Main";
+            this.Text = "VIAR MetaForge";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMetadatos)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Button btnUrl;
+        private System.Windows.Forms.TextBox tbURLImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvMetadatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colMetadata;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colValue;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Label lMaps;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ MetaForge/Main.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Contracts;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetaForge.Utilities;
+
+namespace MetaForge
+{
+    public partial class Main : Form
+    {
+        
+        object[] GetMetadata(PropertyItem data)
+        {
+            object[] item = new object[2];
+            item[0] = Dictionaries.MetadataNames[data.Id];
+            switch (data.Id)
+            {
+                case 2:
+                    item[1] = Bytes2Degrees(data.Value);
+                    break;
+                case 4:
+                    item[1] = Bytes2Degrees(data.Value);
+                    break;
+                case 5:
+                    item[1] = Dictionaries.AltitudRef[int.Parse(DecodePropertyValue(data))];
+                    break;
+                case 6:
+                    item[1] = Bytes2Degrees(data.Value);
+                    break;
+                case 274:
+                    item[1] = Dictionaries.Orientacion[int.Parse(DecodePropertyValue(data))];
+                    break;
+                default:
+                    item[1] = DecodePropertyValue(data);
+                    break;
+            }
+            return item;
+        }
+        void ListMetadata()
+        {
+            Image image = (Bitmap)pbImage.Image.Clone();
+            foreach (PropertyItem property in image.PropertyItems)
+            {
+                try
+                {
+                    dgvMetadatos.Rows.Add(GetMetadata(property));
+                }
+                catch
+                { 
+                    //
+                }
+            }
+            try
+            {
+                double latitud = Bytes2Degrees(image.GetPropertyItem(2).Value, image.GetPropertyItem(1).Value);
+                double longitud = Bytes2Degrees(image.GetPropertyItem(4).Value, image.GetPropertyItem(3).Value);
+                string URLGoogleMaps = $"https://www.google.com/maps/@{latitud},{longitud}z?entry=ttu";
+                lMaps.Text = URLGoogleMaps;
+            }
+            catch
+            {
+                lMaps.Text = "";
+            }
+        }
+
+        string DecodePropertyValue(PropertyItem property)
+        {
+            // La interpretación del valor depende del tipo de la propiedad
+            // Este es un ejemplo básico que intenta decodificar valores comunes
+            switch (property.Type)
+            {
+                case 1: // byte
+                    if (property.Len == 1)
+                    {
+                        return property.Value[0].ToString();
+                    }
+                    return BitConverter.ToString(property.Value);
+                case 2: // ASCII string
+                    return Encoding.ASCII.GetString(property.Value).Trim('\0');
+                case 3: // short
+                    if (property.Len == 2)
+                    {
+                        return BitConverter.ToInt16(property.Value, 0).ToString();
+                    }
+                    break;
+                case 4: // long
+                    if (property.Len == 4)
+                    {
+                        return BitConverter.ToInt32(property.Value, 0).ToString();
+                    }
+                    break;
+                case 5: // rational
+                        // Un rational se compone de dos longs, el numerador y el denominador
+                    if (property.Len == 8)
+                    {
+                        uint numerator = BitConverter.ToUInt32(property.Value, 0);
+                        uint denominator = BitConverter.ToUInt32(property.Value, 4);
+                        return $"{numerator}/{denominator}";
+                    }
+                    break;
+                    // Puedes añadir más casos para otros tipos si es necesario
+            }
+            return "No se pudo interpretar el valor";
+        }
+
+        double Bytes2Degrees(byte[] bytes, byte[] orientation)
+        {
+            // Convierte los bytes en grados, minutos y segundos
+            double degrees = BitConverter.ToInt32(bytes, 0) / (double)BitConverter.ToInt32(bytes, 4);
+            double minutes = BitConverter.ToInt32(bytes, 8) / (double)BitConverter.ToInt32(bytes, 12);
+            double seconds = BitConverter.ToInt32(bytes, 16) / (double)BitConverter.ToInt32(bytes, 20);
+
+            degrees += (minutes / 60) + (seconds / 3600);
+            return orientation[0] == 'W' || orientation[0] == 'S' ? -degrees: degrees;
+        }
+        double Bytes2Degrees(byte[] bytes)
+        {
+            // Convierte los bytes en grados, minutos y segundos
+            double degrees = BitConverter.ToInt32(bytes, 0) / (double)BitConverter.ToInt32(bytes, 4);
+            double minutes = BitConverter.ToInt32(bytes, 8) / (double)BitConverter.ToInt32(bytes, 12);
+            double seconds = BitConverter.ToInt32(bytes, 16) / (double)BitConverter.ToInt32(bytes, 20);
+
+            // Convierte a grados decimales
+            return degrees + (minutes / 60) + (seconds / 3600);
+        }
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void pbImage_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            
+        }
+
+        private void btnUrl_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tbURLImage.Text = openFileDialog1.FileName;
+                tbURLImage_KeyPress(btnUrl, new KeyPressEventArgs((char)13));
+            }
+        }
+
+
+        private void pbImage_Validated(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (lMaps.Text != "SIN UBICACION")
+            {
+                Clipboard.SetText(lMaps.Text);
+                toolTip1.Show("Se ha copiado la liga", btnCopy, 3000);
+            }
+        }
+
+        private void tbURLImage_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                if (System.IO.File.Exists(tbURLImage.Text))
+                {
+                    try
+                    {
+                        pbImage.Image = Image.FromFile(tbURLImage.Text);
+                        ListMetadata();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("No se puede cargar la imagen", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("La ruta no es un archivo valido", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
